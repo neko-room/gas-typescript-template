d02b0dc7d38d50e8c2a3cbe1ad874a43583bd5eb
nekodayo2222 <99164438+nekodayo2222@users.noreply.github.com>
2024-10-07 05:10:18 +0900

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// https://prettier.io/docs/en/configuration.html
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+}
+
+module.exports = config
